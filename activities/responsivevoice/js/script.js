07522e1b0aa3209a1b7022e5c6f6931617418c14
olstinker <giazozula@gmail.com>
2022-01-30 14:37:48 -0500

    Followed Activities for the week

--- activities/responsivevoice/js/script.js
+++ activities/responsivevoice/js/script.js
@@ -8,26 +8,40 @@ author, and this description to match your project!
 
 "use strict";
 
+let on = false;
 
 /**
 Description of preload
 */
-function preload() {
-
-}
-
+function preload() {}
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(500, 500);
 
+  if (annyang) {
+    let commands = {
+      "turn the light on": function () {
+        on = true;
+      },
+      "turn the light off": function () {
+        on = false;
+      },
+    };
+    annyang.addCommands(commands);
+    annyang.start();
+  }
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
-
+  if (on) {
+    background(255);
+  } else {
+    background(0);
+  }
 }
