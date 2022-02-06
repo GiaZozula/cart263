161ea74ff9a51a8df8310b025f0edda08b675416
olstinker <giazozula@gmail.com>
2022-02-05 21:38:06 -0500

    Starting the first step for the exercise
    - Adding extra JSON reference links
    - Adding more properties to the profile, along with corresponding text

--- activities/spy-profile-generator/js/script.js
+++ activities/spy-profile-generator/js/script.js
@@ -3,6 +3,8 @@ SPY PROFILE GENERATOR
 Gia <3
 
 Generates a randomized spy profile for the user, and password protects it.
+
+perspective is my password :)
 */
 
 "use strict";
@@ -11,12 +13,19 @@ let spyProfile = {
   name: `**REDACTED**`,
   alias: `**REDACTED**`,
   secretWeapon: `**REDACTED**`,
+  safeHouseLocation: `**REDACTED**`,
+  currentBenefactor: `**REDACTED**`,
+  currentTarget: `**REDACTED**`,
   password: `**REDACTED**`,
 };
 
 let instrumentData = undefined;
 let objectData = undefined;
 let tarotData = undefined;
+let countriesData = undefined;
+let richPeopleData = undefined;
+let corporationsData = undefined;
+let secretSocietiesData = undefined;
 
 function preload() {
   instrumentData = loadJSON(
@@ -27,6 +36,18 @@ function preload() {
   );
   tarotData = loadJSON(
     `https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`
+  );
+  countriesData = loadJSON(
+    `https://raw.githubusercontent.com/dariusk/corpora/master/data/geography/countries_with_capitals.json`
+  );
+  richpeopleData = loadJSON(
+    `https://raw.githubusercontent.com/dariusk/corpora/master/data/humans/richpeople.json`
+  );
+  corporationsData = loadJSON(
+    `https://github.com/dariusk/corpora/blob/master/data/corporations/fortune500.json`
+  );
+  secretSocietiesData = loadJSON(
+    `https://raw.githubusercontent.com/dariusk/corpora/master/data/societies_and_groups/semi_secret.json`
   );
 }
 
@@ -41,6 +62,9 @@ function setup() {
       spyProfile.alias = data.alias;
       spyProfile.secretWeapon = data.secretWeapon;
       spyProfile.password = data.password;
+      spyProfile.safeHouseLocation = data.safeHouseLocation;
+      spyProfile.currentBenefactor = data.currentBenefactor;
+      spyProfile.currentTarget = data.currentTarget;
     }
   } else {
     generateSpyProfile();
@@ -54,6 +78,16 @@ function generateSpyProfile() {
   spyProfile.secretWeapon = random(objectData.objects);
   let card = random(tarotData.tarot_interpretations);
   spyProfile.password = random(card.keywords);
+  let safeHouse = random(countriesData.countries);
+  spyProfile.safeHouseLocation = `${safeHouse}`;
+  let currentBenefactor = random(richpeopleData.richPeople.name);
+  spyProfile.currentBenefactor = `${currentBenefactor}`;
+  let benefactorOrg = random(secretSocietiesData);
+  spyProfile.benefactorOrg = `${benefactorOrg}`;
+  let currentTarget = random(richpeopleData.richPeople.name);
+  spyProfile.currentTarget = `${currentTarget}`;
+  let targetOrg = random(corporationsData.companies);
+  spyProfile.targetOrg = `${targetOrg}`;
 
   localStorage.setItem(`spy-profile-data`, JSON.stringify(spyProfile));
 }
@@ -66,7 +100,14 @@ function draw() {
 Name: ${spyProfile.name}
 Alias: ${spyProfile.alias}
 Secret Weapon: ${spyProfile.secretWeapon}
-Password: ${spyProfile.password}`;
+Password: ${spyProfile.password}
+Safe House Location: ${spyProfile.safeHouseLocation}
+Current Benefactor: ${spyProfile.currentBenefactor}
+Benefactor Organization:
+Current Target: ${spyProfile.currentTarget}
+Target Organization:
+
+`;
 
   push();
   textFont(`Courier, monospace`);
