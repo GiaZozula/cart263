5cb580b810c8f961476239a8258a532b7b138112
olstinker <giazozula@gmail.com>
2022-02-24 12:12:17 -0500

    Project 1 - Starting with the code written by Pippin during our meeting
    - Now to expand upon this base!

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -6,17 +6,33 @@ Gia <3
 
 "use strict";
 
-/**
-Description of preload
-*/
-function preload() {}
+let dog = {
+  x: 100,
+  y: 0,
+  vy: 0,
+  sound: undefined,
+};
 
-/**
-Description of setup
-*/
-function setup() {}
+function preload() {
+  dog.sound = loadSound(`assets/sounds/bark.wav`);
+}
 
-/**
-Description of draw()
-*/
-function draw() {}
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(0);
+
+  let volume = map(dist(mouseX, mouseY, dog.x, dog.y), 0, 250, 1, 0);
+  volume = constrain(volume, 0, 1);
+  dog.sound.setVolume(volume);
+
+  ellipse(dog.x, dog.y, 100);
+  dog.y += dog.vy;
+}
+
+function mousePressed() {
+  dog.sound.loop();
+  dog.vy = 1;
+}
