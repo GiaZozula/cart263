44eb66285a25cf120358e90c7b299af7c08e8ad8
olstinker <giazozula@gmail.com>
2022-02-24 12:53:57 -0500

    Project 1 - Further setting up the AudioObject class
    - Added coordinates, velocity, and sound

---
+++ projects/project1/js/AudioObject.js
@@ -0,0 +1,9 @@
+class AudioObject {
+  //basic properties of the AudioObject class
+  constructor(x, y, vx, sound) {
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.sound = undefined;
+  }
+}
