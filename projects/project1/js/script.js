abd2bd8cd555c8e7e6dade10ff716acd4390b7d5
olstinker <giazozula@gmail.com>
2022-02-24 12:05:44 -0500

    Project 1 - Setting up the file structure
    Getting started!

---
+++ projects/project1/js/script.js
@@ -0,0 +1,22 @@
+/**
+Project 1 - The Conversation
+Gia <3
+
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {}
+
+/**
+Description of setup
+*/
+function setup() {}
+
+/**
+Description of draw()
+*/
+function draw() {}
