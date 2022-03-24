ffda8dbe2c1f4b35d42154a2d6213de0e688d27d
olstinker <giazozula@gmail.com>
2022-03-24 13:34:41 -0400

    Completed the activity!

--- activities/code-taker/js/script.js
+++ activities/code-taker/js/script.js
@@ -1,11 +1,38 @@
 /**
-Title of Project
-Author Name
+Code Taker
+Gia
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Da Vinci?!
 */
 
 "use strict";
 
-// Code goes here
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function () {
+      $(this).dialog(`close`);
+    },
+  },
+});
+
+$(`.secret`).one(`mouseover`, function (event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({
+    helper: `clone`,
+  });
+});
+
+$(`#answer`).droppable({
+  drop: function (event, ui) {
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+
+    //check if they got it
+    if ($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  },
+});
