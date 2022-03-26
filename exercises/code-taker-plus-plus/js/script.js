7c2545636da783316c3c12dfe66657e3f7e13424
olstinker <giazozula@gmail.com>
2022-03-25 20:09:15 -0400

    Added some finishing touches with a comment, a style change,  and a new button text

--- exercises/code-taker-plus-plus/js/script.js
+++ exercises/code-taker-plus-plus/js/script.js
@@ -1,8 +1,9 @@
 /**
-Code Taker
-Gia
+Code Taker Plus Plus
+by Gia
 
-Da Vinci?!
+What first appears to be a bunch of barcodes is secretly a beautiful poem!
+
 */
 
 "use strict";
@@ -10,7 +11,7 @@ Da Vinci?!
 $(`#solved-dialog`).dialog({
   autoOpen: false,
   buttons: {
-    "I know.": function () {
+    "time to read": function () {
       $(this).dialog(`close`);
     },
   },
@@ -30,9 +31,10 @@ $(`#answer`).droppable({
     ui.draggable.draggable(`disable`);
     ui.draggable.removeClass(`found`);
 
-    //check if they got it
+    //check if they got it, and if so change the font so it is legible!
     if ($(this).text() === `futurity`) {
       document.getElementById("poem").style.fontFamily = "monospace,sans-serif";
+      document.getElementById("poem").style.fontSize = "1.6rem";
       document.getElementById("answer").style.fontFamily =
         "monospace,sans-serif";
       document.getElementById("answer").style.writingMode = "vertical-lr";
