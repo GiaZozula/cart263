5f48b3dabf6144b93cc15c8ba20978616c60fbfe
olstinker <giazozula@gmail.com>
2022-03-27 19:33:42 -0400

    Setting things up and adding my AudioObject code from Project 1

---
+++ projects/Project 2 Prototype/js/AudioObject.js
@@ -0,0 +1,55 @@
+class AudioObject {
+  //basic properties of the AudioObject class
+  constructor(x, y, sound) {
+    this.x = x;
+    this.y = y;
+    this.vx = 2;
+    this.vy = 2;
+    this.sound = sound;
+    this.size = 200;
+    this.speed = 3.25;
+    this.changeDirection = 0.2;
+    this.isPlaying = false;
+    this.isControlled = false;
+  }
+
+  //check to see if it is playing!
+  play() {
+    if (!this.isPlaying) {
+      this.sound.loop();
+      this.isPlaying = true;
+    }
+  }
+
+  //adding a stop ability for the end state
+  stop() {
+    if (this.isPlaying) {
+      this.sound.stop();
+      this.isPlaying = false;
+    }
+  }
+
+  move() {
+    // First have a randomized chance of direction change
+    let r = random(0, 1);
+    if (r < this.changeDirection) {
+      this.vx = random(-this.speed, this.speed);
+      this.vy = random(-this.speed, this.speed);
+    }
+
+    // add the velocity to the positon to get move the object
+    this.x += this.vx;
+    this.y += this.vy;
+
+    // Constrain to the bounds of the canvas
+    this.x = constrain(this.x, 0, width);
+    this.y = constrain(this.y, 0, height);
+  }
+
+  //Pippin's wonderful code that got me started on spatial audio
+  spatialVolume() {
+    let volume = map(dist(mouseX, mouseY, this.x, this.y), 0, 250, 1, 0);
+    volume = constrain(volume, 0, 1);
+    this.sound.setVolume(volume);
+  }
+}
---
+++ projects/Project 2 Prototype/js/script.js
@@ -0,0 +1,27 @@
+/**
+Project 2 Prototype
+Gia
+
+
+*/
+
+"use strict";
+
+//defining audioObejcts array and properties
+let audioObjects = [];
+let numAudioObjects = 10;
+
+/**
+Description of preload
+*/
+function preload() {}
+
+/**
+Description of setup
+*/
+function setup() {}
+
+/**
+Description of draw()
+*/
+function draw() {}
