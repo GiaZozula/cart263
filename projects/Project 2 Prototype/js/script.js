20335fa7112993719f6693a62a07bb26350d9c4b
olstinker <giazozula@gmail.com>
2022-03-27 19:39:30 -0400

    Added some new objects, and started setting up skeleton control schemes for both viewers

--- projects/Project 2 Prototype/js/script.js
+++ projects/Project 2 Prototype/js/script.js
@@ -7,9 +7,21 @@ Gia
 
 "use strict";
 
+//this determines which viewer the user is currently in control of
+let viewer1 = true;
+let viewer2 = false;
+
 //defining audioObejcts array and properties
 let audioObjects = [];
 let numAudioObjects = 10;
+
+let userAudioObject;
+
+//defining directions for veiwer1 mode
+let north = true;
+let south = false;
+let east = false;
+let west = false;
 
 /**
 Description of preload
@@ -25,3 +37,19 @@ function setup() {}
 Description of draw()
 */
 function draw() {}
+
+//this handles the controls when the user is in Viewer1 mode
+function viewer1Input() {
+  if (keyIsDown(RIGHT_ARROW)) {
+  } else if (keyIsDown(LEFT_ARROW)) {
+  }
+}
+
+//this handles the controls when the user is in Viewer2 mode
+function viewer2Input() {
+  if (keyIsDown(RIGHT_ARROW)) {
+  } else if (keyIsDown(LEFT_ARROW)) {
+  } else if (keyIsDown(UP_ARROW)) {
+  } else if (keyIsDown(DOWN_ARROW)) {
+  }
+}
