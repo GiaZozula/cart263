13410e689ebfbbc2a5c700238d010659ab965e48
olstinker <giazozula@gmail.com>
2022-03-31 19:11:44 -0400

    Finished the activity :)

--- activities/desperately-seeking-sadness/js/Boot.js
+++ activities/desperately-seeking-sadness/js/Boot.js
@@ -8,7 +8,8 @@ class Boot extends Phaser.Scene {
   preload() {
     //load assets here!
     this.load.image(`avatar`, `assets/images/avatar.png`);
-
+    this.load.image(`thumbs-down`, `assets/images/thumbs-down.png`);
+    this.load.image(`thumbs-up`, `assets/images/thumbs-up.png`);
     this.load.on(`complete`, () => {
       this.scene.start(`play`);
     });
